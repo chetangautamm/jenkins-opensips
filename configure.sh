05e0ad5b371ed7b2344f14c07def6b7a62a850d4
chetangautamm <71440767+chetangautamm@users.noreply.github.com>
2021-02-26 18:03:44 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -29,5 +29,5 @@ export DISPLAY=:0.0;
 gnome-terminal "kubectl exec -it $uas -n default -- bash -c 'cd /home/sipp/sipp-3.4.1 && ./sipp -bg -sf uas_mod_orig.xml -rsa $opensips_ip:5060 -i $uas_ip -p 5080' ; "
 #kubectl exec -i $uac -n default -- bash -c "echo $?" ;
 #start uac
-gnome-terminal "kubectl exec -it $uac -n default -- bash -c 'cd /home/sipp/sipp-3.4.1 && ./sipp -bg -sf uac_mod.xml $opensips_ip:5060 -trace_screen -s chetan -i $uac_ip -p 5065  -m 100 -r 10 -rp 1000' ;"
+gnome-terminal "kubectl exec -it $uac -n default -- bash -c 'cd /home/sipp/sipp-3.4.1 && ./sipp -bg -sf uac_mod.xml $opensips_ip:5060 -trace_screen -trace_logs -s chetan -i $uac_ip -p 5065  -m 100 -r 10 -rp 1000' ;"
 #kubectl exec -i $uac -n default -- bash -c "echo $?" ;
