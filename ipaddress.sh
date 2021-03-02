41a3c0d6c688a8dce183d692a2c61c43a804ccb7
chetangautamm <71440767+chetangautamm@users.noreply.github.com>
2021-03-02 11:06:17 +0530

    Update ipaddress.sh

--- ipaddress.sh
+++ ipaddress.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+#fetch required data
 opensips_server=$(kubectl get pods -n default -o wide | grep opensips | awk '{print $1}');
 opensips_ip=$(kubectl get pods -n default -o wide | grep opensips | awk '{print $6}');
 uac=$(kubectl get pods -n default -o wide | grep 'uac' | awk '{print $1}'); 
 uac_ip=$(kubectl get pods -n default -o wide | grep 'uac' | awk '{print $6}');
 
-sip=$opensips_ip
-uac=$uac_ip
-
-sed -i "s/172.17.0.2/$uac/g" uac_config.yaml
-sed -i "s/172.17.0.2/$uac/g" uas_config.yaml
-sed -i "s/OPENSIPS/$sip/g" uas_config.yaml
+#putting data in configuration
+#sed -i "s/172.17.0.2/$uac/g" uac_config.yaml
+sed -i "s/UAC/$uac_ip/g" uas_config.yaml
+sed -i "s/OPENSIPS/$opensips_ip/g" uas_config.yaml
