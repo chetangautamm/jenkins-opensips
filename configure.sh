2a36fbf6fe9c8a5f2c32c47ed6ff08b4010c4e5a
chetangautamm <71440767+chetangautamm@users.noreply.github.com>
2021-03-02 18:03:25 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -25,8 +25,8 @@ kubectl exec -i $uas -n default -- bash -c "sed -i -e 's/172.21.112.222/($uac_ip
 kubectl exec -i $uac -n default -- bash -c "sed -i -e 's/172.16.0.10/($uas_ip)/g' /home/sipp/sipp-3.4.1/uac_mod.xml" ;
 
 #start uas
-kubectl exec -i $uas -n default -- bash -c "export TERM=xterm && ./sipp -sf uas_mod_orig.xml -rsa $opensips_ip:5060 -i $uas_ip -p 5080" ; 
+kubectl exec -i $uas -n default -- bash -c "export TERM=xterm && ./sipp -sf uas_mod_orig.xml -rsa $opensips_ip:5060 -i $uas_ip -p 5080 &" ; 
 #kubectl exec -i $uas -n default -- bash -c "echo $?" ;
 #start uac
-kubectl exec -i $uac -n default -- bash -c "export TERM=xterm && ./sipp -sf uac_mod.xml $opensips_ip:5060 -trace_screen -s chetan -i $uac_ip -p 5065  -m 100 -r 10 -rp 1000 &" ;
+kubectl exec -i $uac -n default -- bash -c "export TERM=xterm && ./sipp -sf uac_mod.xml $opensips_ip:5060 -s chetan -i $uac_ip -p 5065  -m 100 -r 10 -rp 1000 " ;
 #kubectl exec -i $uac -n default -- bash -c "echo $?" ;
