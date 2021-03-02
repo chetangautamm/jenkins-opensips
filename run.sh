0ef60048a7e77f5df78d4655632780fc353af721
chetangautamm <71440767+chetangautamm@users.noreply.github.com>
2021-03-02 13:21:38 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 uas=$(kubectl get pods -n default -o wide | grep 'uas' | awk '{print $1}');
-kubectl exec  $uas  sh shell/uas.sh
+kubectl exec  $uas  sh /home/sipp/sipp-3.4.1/shell/uas.sh
