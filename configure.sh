77b0748759bbc0f2383a8be4cae9258a1f091ef3
chetangautamm <71440767+chetangautamm@users.noreply.github.com>
2021-03-03 14:31:09 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -8,15 +8,15 @@ uac_ip=$(kubectl get pods -n default -o wide | grep 'uac' | awk '{print $6}');
 uas_ip=$(kubectl get pods -n default -o wide | grep 'uas' | awk '{print $6}');
 
 #edit opensips server pod
-kubectl exec -i $opensips_server -n default -- bash -c "sed -i 's/172.17.0.2/$opensips_ip/g' /usr/local/opensips_proxy/etc/opensips/opensips_residential_*"
-kubectl exec -i $opensips_server -n default -- bash -c "cd /usr/local/opensips_proxy/sbin/"
-kubectl exec -i $opensips_server -n default -- bash -c "/etc/init.d/mysql start"
-kubectl exec -i $opensips_server -n default -- bash -c "/etc/init.d/opensips start"
+kubectl exec -i $opensips_server -n default -- bash -c "sed -i 's/172.17.0.2/$opensips_ip/g' /usr/local/opensips_proxy/etc/opensips/opensips_residential_*" ;
+kubectl exec -i $opensips_server -n default -- bash -c "cd /usr/local/opensips_proxy/sbin/" ;
+kubectl exec -i $opensips_server -n default -- bash -c "/etc/init.d/mysql start" ;
+kubectl exec -i $opensips_server -n default -- bash -c "/etc/init.d/opensips start" ;
 
 #add user
-kubectl exec -i $opensips_server -n default -- bash -c "cd /usr/local/opensips_proxy/sbin/ && ./opensipsctl ul add chetan sip:chetan@$uas_ip:5080"
-kubectl exec -i $opensips_server -n default -- bash -c "/etc/init.d/opensips restart"
-kubectl exec -i $opensips_server -n default -- bash -c "/etc/init.d/opensips status"
+kubectl exec -i $opensips_server -n default -- bash -c "cd /usr/local/opensips_proxy/sbin/ && ./opensipsctl ul add chetan sip:chetan@$uas_ip:5080" ;
+kubectl exec -i $opensips_server -n default -- bash -c "/etc/init.d/opensips restart" ;
+kubectl exec -i $opensips_server -n default -- bash -c "/etc/init.d/opensips status && echo 'successsful'" ;
 
 #edit uas pod
 kubectl exec -i $uas -n default -- bash -c "sed -i -e 's/172.21.112.222/$uac_ip/g' /home/sipp/sipp-3.4.1/uas_mod_orig.xml" ;
